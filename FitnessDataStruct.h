72fdf0c6d342f923167427d55cdd3dfae46c61f9
mmariam-ali <146266404+mmariam-ali@users.noreply.github.com>
2023-11-18 22:37:14 +0000

    progress

---
+++ FitnessDataStruct.h
@@ -0,0 +1,83 @@
+
+#ifndef FITNESS_DATA_STRUCT_H
+#define FITNESS_DATA_STRUCT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+//#define buffer_size 256
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Helper function prototypes
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
+
+
+
+
+/**
+* @brief Opens the file in the correct mode
+*
+* @param filename the name of the file to open
+* @param mode the mode (r/w/a/r+/w+/a+)
+* @return FILE* The file object to store the opened file in.
+*/  
+
+
+//setting filename
+char *input_filename() {
+    //char filename;
+    int buffer_size =100;
+    char line[buffer_size];
+    char filename[buffer_size];
+    
+        printf("Input filename: \n");
+        fgets(line, buffer_size, stdin);
+        sscanf(line, " %s%*s", filename);
+        https://stackoverflow.com/questions/38030681/when-i-put-a-scanf-inside-of-a-switch-case-with-type-char-it-skips-it-entirely-w
+        
+        return filename;
+    //openfile(filename, "r");
+}
+
+//opening file
+FILE *open_file(char *filename, char *mode) {
+    // printf("Input filename: \n");
+    // scanf("%c", &filename);
+    FILE *file= fopen(filename, "r");   //open file in read mode and check it isn't empty
+        if (file ==NULL) {
+            perror("Error opening file: file is empty. \n");
+            return 1;
+        }
+
+}
+
+/**
+ * @brief Reads the data from the input file into an array of structs
+ *
+ * @param inputFile the open file object
+ * @param dataArray the array of readings
+ * @return int Returns the number of readings from the file
+ */
+
+// int read_file(FILE *inputFile, FITNESS_DATA, char *mode) {
+//     FITNESS_DATA fitness_data[9999];   //create struct array 
+
+//         while (fgets(buffer, buffer_size, file)!= NULL) {   //while the line is not empty
+//             line_count++;   //counting the number of lines in the file
+
+//             tokeniseRecord(buffer, ",", date, time, steps);    //splitting the line into date, time, steps
+//             strcpy(fitness_data[i].date, date);                //assigning date, time and steps to data struct 
+//             strcpy(fitness_data[i].time, time);
+//             strcpy(fitness_data[i].steps, steps);
+//             i++;                                               //does this for every i; every line in the file
+//         }
+
+
+#endif // FITNESS_DATA_STRUCT_H
---
+++ StepCounter_Final.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FitnessDataStruct.h"
+
+// Struct moved to header file
+
+// Define any additional variables here
+// Global variables for filename and FITNESS_DATA array
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+
+
+
+// Complete the main function
+    
+int main() {
+    char choice, filename;
+    char record, date[11], time[6], steps[10]; 
+    int intsteps, line_count=0, i;
+    int buffer_size=256;    //creating the size of the buffer
+    char buffer[buffer_size];   //creating buffer and its size
+
+
+
+        printf("Menu options:\n");
+        printf("A: Specify the filename to be imported\n");                      
+        printf("B: Display the total number of records in the file\n");                   
+        printf("C: Find the date and time of the timeslot with the fewest steps \n");                    
+        printf("D: Find the date and time of the timeslot with the largest number of steps\n");                    
+        printf("E: Find the mean step count of all the records in the file \n");       
+        printf("F: Find the longest continuous period where the step count is above 500 steps \n");        
+        printf("Q: Quit \n");
+
+        printf("Enter choice: ");
+        scanf("%c", &choice);
+        
+        //choice= toupper(choice);
+
+    // if (choice == 'A') {
+    //     int buffer_size =100;
+    //     char line[buffer_size];
+    //     char filename[buffer_size];
+    
+    //     printf("Input filename: \n");
+    //     fgets(line, buffer_size, stdin);
+    //     sscanf(line, " %s", filename);
+
+    // }
+
+    switch (choice){
+        case 'A':    //sort this out
+            input_filename();
+            FILE *input = fopen(filename, "r");
+            break;
+
+        case 'B':    
+        printf("You have chosen choice B\n");
+            break;
+
+        case 'C':
+            printf("You have chosen choice C\n");
+            break;
+
+        case 'D':  
+            printf("You have chosen choice D\n");
+            break;
+
+        case 'E':   
+            printf("You have chosen choice E\n");
+            break;
+
+        case 'F': 
+            printf("You have chosen choice F");
+            break;
+
+        case 'Q': 
+            printf("You have chosen choice Q\n");
+            break;
+            
+        default: printf ("Invalid choice. Try again.\n");
+    }
+}
+   
+
+
+
+
+
+
+
+
+
+
